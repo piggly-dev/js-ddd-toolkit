88207f93538665bb6c8cfc71fca3d374cf5e4eb9
Caique Araujo <caique@studiopiggly.com.br>
2023-05-31 14:44:43 -0300

    :sparkles: feat(events): All classes for domain events

---
+++ src/core/DomainEvent.ts
@@ -0,0 +1,22 @@
+import { EventPayload } from '@piggly/event-bus';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * @file Base domain event class to be used/extended by all domain events.
+ * @copyright Piggly Lab 2023
+ */
+export default class DomainEvent<Payload> extends EventPayload<Payload> {
+	/**
+	 * Will generate a new uuidv4 for default id.
+	 *
+	 * @param {Entity<Props>} object
+	 * @returns {boolean}
+	 * @public
+	 * @memberof Entity
+	 * @since 1.0.0
+	 * @author Caique Araujo <caique@piggly.com.br>
+	 */
+	public generateId(): string {
+		return uuidv4();
+	}
+}
